fd7c4fd9bbfb19a4fa1ca9e2b2f0b357e59ed4fa
gmgngja <kristina-novikova13@rambler.ru>
2019-09-02 21:59:44 +0300

    Добавлен ассерт на смену состояния чекбокса после первого клика

--- checkboxTest.py
+++ checkboxTest.py
@@ -1,28 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 import time
 
 
-class CkeckboxTest(unittest.TestCase):
+class CheckboxTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
     def test_search(self):
         self.driver.get("http://blog.csssr.ru/qa-engineer/")
 
-        continue_link = self.driver.find_element_by_link_text('НАХОДИТЬ НЕСОВЕРШЕНСТВА')
+        continue_link = self.driver.find_element_by_link_text('СОПРОВОЖДАТЬ ПРОЕКТЫ')
         continue_link.click()
         time.sleep(3)
-        checkbox = self.driver.find_element_by_id("beautiful")
+
+        checkbox = self.driver.find_element_by_id("attention2")
         state = checkbox.get_property("checked")
-        label = self.driver.find_element_by_xpath("//label[@for='beautiful']")
+        label = self.driver.find_element_by_xpath("//label[@for='attention2']")
 
         label.click()
-        label.click()
-
         updated_state = checkbox.get_property("checked")
-        assert updated_state == state, "checkbox wasn't returned to origin state after double click"
+        assert updated_state != state, "Checkbox did not change state"
+
+        label.click()
+        updated_state = checkbox.get_property("checked")
+        assert updated_state == state, "Checkbox did not returned to origin after double click"
 
     def tearDown(self):
         self.driver.close()
